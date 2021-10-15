af4011632a1fc8c1fa773f13c174392ab671d7df
Isaque O. Silva <Isaque1945@hotmail.com>
2021-10-15 17:21:20 -0300

    Site - Produtos

--- admin-products.php
+++ admin-products.php
@@ -82,7 +82,7 @@ $app->post('/admin/products/:idproduct', function($idproduct) {
 
 	$product->save();
 
-	$product->setPhoto($_FILES["file"]);
+	$product->setPhoto($_FILES['file']);
 
 	header('Location: /admin/products');
 	exit;
