776cba5e50b788cb310540c46c13a915ea877072
Isaque O. Silva <Isaque1945@hotmail.com>
2021-10-23 11:18:00 -0300

    Minha Conta

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -13,6 +13,7 @@ class User extends Model {
     const SECRET_IV         = 'HcodePhp7_Secret_IV';
     const ERROR             = 'UserError';
     const ERROR_REGISTER    = 'UserErrorRegister';
+    const SUCCESS           = 'UserSuccess';
 
     public static function getFromSession() {
 
@@ -60,7 +61,7 @@ class User extends Model {
 
         $sql = new Sql();
 
-        $results = $sql->select('SELECT * FROM tb_users WHERE (deslogin = :LOGIN);', array(
+        $results = $sql->select('SELECT * FROM tb_users a INNER JOIN tb_persons b ON a.idperson = b.idperson WHERE (a.deslogin = :LOGIN);', array(
             ':LOGIN' => $login
         ));
 
@@ -346,5 +347,27 @@ class User extends Model {
 
         return (count($results) > 0);
     }
+
+    public static function setSuccess($msg) {
+
+		$_SESSION[User::SUCCESS] = $msg;
+
+	}
+
+	public static function getSuccess() {
+
+		$msg = (isset($_SESSION[User::SUCCESS]) && $_SESSION[User::SUCCESS]) ? $_SESSION[User::SUCCESS] : '';
+
+		User::clearSuccess();
+
+		return $msg;
+
+	}
+
+	public static function clearSuccess() {
+
+		$_SESSION[User::SUCCESS] = NULL;
+
+	}
 }
 ?>
